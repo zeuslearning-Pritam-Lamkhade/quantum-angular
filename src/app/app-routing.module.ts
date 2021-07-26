35ac8436b0947875b12144983600ffec8c6f6fce
zeuslearning-Pritam-Lamkhade <86939627+zeuslearning-Pritam-Lamkhade@users.noreply.github.com>
2021-07-26 14:06:16 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:'dashboard',component:DashboardComponent},
+  {
+    path:'',component:LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
